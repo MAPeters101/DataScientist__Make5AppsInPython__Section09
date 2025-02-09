6303ae61e8abc67f9a56b99e2544dcd1ba1cbd7a
Mark Peters <mapeters101@earthlink.net>
2025-02-09 00:56:00 -0500

    Initial code for work on lesson110 for game.py

--- game.py
+++ game.py
@@ -2,8 +2,6 @@ import pygame
 pygame.init()
 window = pygame.display.set_mode([450,450])
 
-image = pygame.image.load("Bugatti.png")
-
 playing = True
 
 while playing:
@@ -12,7 +10,6 @@ while playing:
             playing = False
 
     window.fill((255,255,255))
-    window.blit(image, (0,0))
 
     pygame.display.update()
 
